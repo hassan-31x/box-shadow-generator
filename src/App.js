feb216327a49b43814386ba28a901abf9ea647df
Hassan <muhammadhassanchannel786@gmail.com>
2022-12-12 23:30:41 +0500

    completed the App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [xaxis, setXaxis] = useState(0)
+  const [yaxis, setYaxis] = useState(0)
+  const [blur, setBlur] = useState(0)
+  const [color, setColor] = useState("black")
+
+  const text = `boxShadow: ${xaxis}px ${yaxis}px ${blur}px ${color};`
+  const copy = async () => {
+    await navigator.clipboard.writeText(text);
+    alert(`(${text}) copied!`);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+
+      <div className="inputs">
+        <label>Horizontal<br/>
+          <input type="range" min='-100' max="100" value={xaxis} onChange={(e) => {setXaxis(e.target.value)}}/>
+        </label><br/>
+        <label>Vertical<br/>
+          <input type="range" min='-100' max="100" value={yaxis} onChange={(e) => {setYaxis(e.target.value)}}/>
+        </label><br/>
+        <label>Blur<br/>
+          <input type="range" min='0' max="100" value={blur} onChange={(e) => {setBlur(e.target.value)}}/>
+        </label><br/>
+        <label>Shadow Color<br/>
+          <input type="color" value={color} onChange={(e) => {setColor(e.target.value)}}/>
+        </label>
+      </div>
+
+      <div className="box" style={{boxShadow: `${xaxis}px ${yaxis}px ${blur}px ${color}`}}>
+        <p onClick={copy}>Copy!</p>
+      </div>
     </div>
   );
 }
-
 export default App;
